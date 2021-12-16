bc840587c1990e3b6dc87216f35ca472823b9ee6
Erdogan <48126890+erdodo@users.noreply.github.com>
2021-12-16 23:43:57 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 /*
 
-Route::get('/', function () {
+Route::get('/hello', function () {
     return view('welcome');
 });
  
 |   get                      => hangi method ile çalışacak 
 |   '/hello'                 => url takibi yapar. "http://localhost/hello" adresine gidildiğinde ne olacak.
-|   return view('welcome');  => çalışacak olan view dosyası return edilir yani ekrana basılır.
+|   return view('world');  => çalışacak olan view dosyası return edilir yani ekrana basılır.
  
 */
 
