dd02136e7add6dc654f8f566c91be160ca7ce266
Erdogan <48126890+erdodo@users.noreply.github.com>
2021-12-16 23:43:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
  
+|   get                      => hangi method ile çalışacak 
 |   '/hello'                 => url takibi yapar. "http://localhost/hello" adresine gidildiğinde ne olacak.
 |   return view('welcome');  => çalışacak olan view dosyası return edilir yani ekrana basılır.
  
