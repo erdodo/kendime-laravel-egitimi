649333e3d8dbaf15bd20d092fbc8ae8edb664b56
Erdogan <48126890+erdodo@users.noreply.github.com>
2021-12-16 23:41:38 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+
+Route::get('/', function () {
+    return view('welcome');
+});
+ 
+|   '/hello'                 => url takibi yapar. "http://localhost/hello" adresine gidildiğinde ne olacak.
+|   return view('welcome');  => çalışacak olan view dosyası return edilir yani ekrana basılır.
+ 
 */
 
 Route::get('/', function () {
